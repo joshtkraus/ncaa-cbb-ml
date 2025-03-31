51954e0a2798767e07330a39cfda2024f98ec77e
Josh Kraus <joshtkraus@gmail.com>
2025-03-31 15:30:29 -0400

    tuning results

--- scripts/utils/GroupedMetrics.py
+++ scripts/utils/GroupedMetrics.py
@@ -1,3 +1,15 @@
+def assign_final(row, c, group_cols, second_highest_dict):
+    # Build key based on number of grouping columns
+    if len(group_cols) == 1:
+        key = row[group_cols[0]]
+    else:
+        key = tuple(row[col] for col in group_cols)
+    
+    if row[c] == row['group_max']:
+        return second_highest_dict.get(key)
+    else:
+        return row['group_max']
+            
 def get_grouped_metrics(df):
     # Libraries
     import pandas as pd
@@ -41,52 +53,77 @@ def get_grouped_metrics(df):
 
     # Calculate
     for c in cols:
+        # Max
         # R32
-        grouped = df.groupby(['Year','Region','R32_Group'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['R32_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year','Region','R32_Group']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['R32_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
         # S16
-        grouped = df.groupby(['Year','Region','S16_Group'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['S16_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year','Region','S16_Group']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['S16_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
         # E8
-        grouped = df.groupby(['Year','Region','E8_Group'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['E8_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year','Region','E8_Group']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['E8_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
         # F4
-        grouped = df.groupby(['Year','Region','Region'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['F4_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year','Region']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['F4_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
         # NCG
-        grouped = df.groupby(['Year','NCG_Group'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['NCG_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year','NCG_Group']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['NCG_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
         # Winner
-        grouped = df.groupby(['Year'])
-        n = grouped[c].transform('count')
-        mean = grouped[c].transform('mean')
-        df['Winner_'+c+'_Avg'] = (mean * n - df[c])/(n-1)
+        group_cols = ['Year']
+        df['group_max'] = df.groupby(group_cols)[c].transform('max')
+        second_highest = df.groupby(group_cols)[c].apply(
+            lambda x: x.nlargest(2).iloc[-1] if len(x) >= 2 else None
+        )
+        second_highest_dict = second_highest.to_dict()
+        df['Winner_'+c+'_Max'] = df.apply(assign_final, args=(c, group_cols, second_highest_dict), axis=1)
+        df.drop(columns=['group_max'], inplace=True)
 
         # Get Difference
         if 'Rank' in c:
-            df['R32_'+c+'_Avg'] = df['R32_'+c+'_Avg'] - df[c]
-            df['S16_'+c+'_Avg'] = df['S16_'+c+'_Avg'] - df[c]
-            df['E8_'+c+'_Avg'] = df['E8_'+c+'_Avg'] - df[c]
-            df['F4_'+c+'_Avg'] = df['F4_'+c+'_Avg'] - df[c]
-            df['NCG_'+c+'_Avg'] = df['NCG_'+c+'_Avg'] - df[c]
-            df['Winner_'+c+'_Avg'] = df['Winner_'+c+'_Avg'] - df[c]
+            df['R32_'+c+'_Max'] = df['R32_'+c+'_Max'] - df[c]
+            df['S16_'+c+'_Max'] = df['S16_'+c+'_Max'] - df[c]
+            df['E8_'+c+'_Max'] = df['E8_'+c+'_Max'] - df[c]
+            df['F4_'+c+'_Max'] = df['F4_'+c+'_Max'] - df[c]
+            df['NCG_'+c+'_Max'] = df['NCG_'+c+'_Max'] - df[c]
+            df['Winner_'+c+'_Max'] = df['Winner_'+c+'_Max'] - df[c]
         else:
-            df['R32_'+c+'_Avg'] = df[c] - df['R32_'+c+'_Avg']
-            df['S16_'+c+'_Avg'] = df[c] - df['S16_'+c+'_Avg']
-            df['E8_'+c+'_Avg'] = df[c] - df['E8_'+c+'_Avg']
-            df['F4_'+c+'_Avg'] = df[c] - df['F4_'+c+'_Avg']
-            df['NCG_'+c+'_Avg'] = df[c] - df['NCG_'+c+'_Avg']
-            df['Winner_'+c+'_Avg'] = df[c] - df['Winner_'+c+'_Avg']
+            df['R32_'+c+'_Max'] = df[c] - df['R32_'+c+'_Max']
+            df['S16_'+c+'_Max'] = df[c] - df['S16_'+c+'_Max']
+            df['E8_'+c+'_Max'] = df[c] - df['E8_'+c+'_Max']
+            df['F4_'+c+'_Max'] = df[c] - df['F4_'+c+'_Max']
+            df['NCG_'+c+'_Max'] = df[c] - df['NCG_'+c+'_Max']
+            df['Winner_'+c+'_Max'] = df[c] - df['Winner_'+c+'_Max']
 
     # Drop
     df.drop(columns=['R32_Group','S16_Group','E8_Group','NCG_Group'],inplace=True)
